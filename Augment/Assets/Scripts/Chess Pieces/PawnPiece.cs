ccf3ce857f87b762d65909ecb5afd04dfe3a41a3
myong003 <matthew.yong0801@gmail.com>
2022-10-29 14:13:06 -0700

    pawn up direction is now positive

--- Augment/Assets/Scripts/Chess Pieces/PawnPiece.cs
+++ Augment/Assets/Scripts/Chess Pieces/PawnPiece.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class PawnPiece : ChessPiece
 {
-    public int direction = 1;   // 1 for moving down, -1 for moving up
+    public int direction = 1;   // 1 for moving up, -1 for moving down
 
     public override void GetPossibleSpaces()
     {
         base.GetPossibleSpaces();
-        int nextY = coord.y + direction;
+        int nextY = coord.y - direction;
 
         if (GameManager.Instance.board.isValidMoveSpace(coord.x, nextY))
         {
             possibleSpaces.Add(new Vector2Int(coord.x, nextY));
             // If pawn is in starting position, also add another space
-            if ((coord.y == 1 && direction > 0) || (coord.y == GameManager.Instance.board.getHeight() - 2 && direction < 0))
+            if ((coord.y == 1 && direction < 0) || (coord.y == GameManager.Instance.board.getHeight() - 2 && direction > 0))
             {
-                if (GameManager.Instance.board.isValidMoveSpace(coord.x, nextY + direction)){
-                    possibleSpaces.Add(new Vector2Int(coord.x, nextY + direction));
+                if (GameManager.Instance.board.isValidMoveSpace(coord.x, nextY - direction)){
+                    possibleSpaces.Add(new Vector2Int(coord.x, nextY - direction));
                 }
             }
         }
