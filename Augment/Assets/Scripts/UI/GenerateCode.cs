fc647b324c743c95eb7237da53df4bf533e1f594
LacieFDC210 <fdc210a@gmail.com>
2022-11-26 20:30:46 -0800

    Push attempt of code generating function

--- Augment/Assets/Scripts/UI/GenerateCode.cs
+++ Augment/Assets/Scripts/UI/GenerateCode.cs
@@ -7,9 +7,14 @@ public class GenerateCode : MonoBehaviour
     public TextMeshProUGUI codeOutput;
     string code = "None";
 
+    void Start()
+    {
+        Generation();
+    }
+
     public void Generation()
     {
-        int codeNum = Random.Range(1, 1000000);
+        int codeNum = Random.Range(0, 999999);
         code = codeNum.ToString("D6");
         codeOutput.text = code;
     }
