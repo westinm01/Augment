a46745f3f514d8b56f9c2095f7946a08cdd556cc
NikOhNo <80977626+NikOhNo@users.noreply.github.com>
2023-01-29 15:19:14 -0800

    Moved Stockfish to Streaming Assets

--- Augment/Assets/Scripts/Player/StockfishAI.cs
+++ Augment/Assets/Scripts/Player/StockfishAI.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using UnityEngine;
+using static UnityEditor.Rendering.CameraUI;
+using System.IO;
+using System.Security.Cryptography;
 
 public class StockfishAI : AIPlayer
 {
     [SerializeField]
     [Range(0, 20)]
     int difficultyLevel;
+
+    private static Process mProcess;
 
     string boardState = "";
 
@@ -21,16 +26,19 @@ public class StockfishAI : AIPlayer
 
     public override IEnumerator MakeMove()
     {
+        UnityEngine.Debug.Log("make move");
+
         // Make a move based off of algebraic notation
         boardState = GetForsythBoardState();
         UnityEngine.Debug.Log(GetBestMove(boardState));
-
+        
         throw new System.NotImplementedException();
     }
 
-    private void Update()
+    private void SendLine(string command)
     {
-        GetForsythBoardState();
+        mProcess.StandardInput.WriteLine(command);
+        mProcess.StandardInput.Flush();
     }
 
     string GetForsythBoardState()
@@ -110,7 +118,7 @@ public class StockfishAI : AIPlayer
 
         currBoardState += ' ';
 
-        // TODO: Say the fullmove clock
+        // Say the fullmove clock
         currBoardState += GameManager.Instance.GetNumFullMoves().ToString();
 
 
@@ -121,10 +129,17 @@ public class StockfishAI : AIPlayer
     string GetBestMove(string forsythEdwardsNotationString)
     {
         var p = new System.Diagnostics.Process();
-        p.StartInfo.FileName = "stockfishExecutable.exe";
+
+        // TODO: find pathname of stockfishExe file and ensure it is included in all builds
+        string pathName = "";
+        pathName += Application.streamingAssetsPath + "/";
+        pathName += "stockfish_15.1_win_x64_avx2/stockfishExecutable";
+
+        p.StartInfo.FileName = pathName;
         p.StartInfo.UseShellExecute = false;
         p.StartInfo.RedirectStandardInput = true;
         p.StartInfo.RedirectStandardOutput = true;
+        p.StartInfo.CreateNoWindow = true;
         p.Start();
         string setupString = "position fen " + forsythEdwardsNotationString;
         p.StandardInput.WriteLine(setupString);
@@ -138,7 +153,7 @@ public class StockfishAI : AIPlayer
         p.StandardInput.WriteLine(processString);
 
         string bestMoveInAlgebraicNotation = p.StandardOutput.ReadLine();
-
+        
         p.Close();
 
         return bestMoveInAlgebraicNotation;
