fcb80827ba8d633a6488b24ca66c896c304fc6d4
myong003 <matthew.yong0801@gmail.com>
2023-01-29 18:55:43 -0800

    Added lobby

---
+++ Augment/Assets/Scripts/Net/LobbyManager.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+using UnityEngine;
+using Unity.Netcode;
+using TMPro;
+
+public class LobbyManager : NetworkBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI inputLobbyCode;
+    [SerializeField]
+    private TextMeshProUGUI numPlayers;
+    [SerializeField]
+    private TextMeshProUGUI lobbyCode;
+    
+    private Lobby hostLobby;
+    private float heartbeatTimer;
+    private async void Start() {
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () => {
+            Debug.Log("Signed in " + AuthenticationService.Instance.PlayerId);
+        };
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    private void Update() {
+        HandleLobbyHeartbeat();
+    }
+
+    private void HandleLobbyHeartbeat() {
+        if (hostLobby != null) {
+            heartbeatTimer -= Time.deltaTime;
+            if (heartbeatTimer < 0f) {
+                float heartbeatTimerMax = 15;
+                heartbeatTimer = heartbeatTimerMax;
+
+                LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id);
+            }
+        }
+    }
+
+    public async void CreateLobby() {
+        try{
+            string lobbyName = "MyLobby";
+            int maxPlayers = 2;
+            CreateLobbyOptions createLobbyOptions = new CreateLobbyOptions {
+                IsPrivate = true,
+            };
+
+            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers, createLobbyOptions);
+            hostLobby = lobby;
+            Debug.Log("Created Lobby! " + lobby.Name + " " + lobby.MaxPlayers + " " + lobby.Id + " " + lobby.LobbyCode);
+            lobbyCode.text = lobby.LobbyCode;
+            numPlayers.text = "1";
+        }
+        catch (LobbyServiceException e) {
+            Debug.Log(e);
+        }
+    }
+
+    public async void ListLobbies() {
+        try {
+            QueryLobbiesOptions queryLobbiesOptions = new QueryLobbiesOptions{
+                Count = 25,
+                Filters  = new List<QueryFilter> {
+                    new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT)
+                },
+                Order = new List<QueryOrder> {
+                    new QueryOrder(false, QueryOrder.FieldOptions.Created)
+                }
+            };
+
+            QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
+            Debug.Log("Lobbies found: " + queryResponse.Results.Count);
+            foreach (Lobby lobby in queryResponse.Results) {
+                Debug.Log(lobby.Name + " " + lobby.MaxPlayers);
+            } 
+        }
+        catch(LobbyServiceException e) {
+            Debug.Log(e);
+        }
+    }
+
+    public void JoinLobbyButton() {
+        JoinLobbyByCode(inputLobbyCode.text);
+    }
+
+    private async void JoinLobbyByCode(string lobbyCode) {
+        try {
+            QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
+
+            await Lobbies.Instance.JoinLobbyByCodeAsync(lobbyCode);
+            Debug.Log("Successfully joined lobby " + lobbyCode);
+            numPlayers.text = "2";
+        } catch (LobbyServiceException e) {
+            Debug.Log(e);
+        }
+    }
+}
