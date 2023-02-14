1b3a373b69474d3f025e8a610da9eac280d5f60a
myong003 <matthew.yong0801@gmail.com>
2023-02-13 20:18:36 -0800

    started lady lemure

---
+++ Augment/Assets/Scripts/Augmentors/LadyLeMure.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadyLeMure : Augmentor
+{
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     name = "Lady LeMure";
+    //     backgroundColor = Color.purple;
+    //     augmentName = "From The Grave";
+    //     augmentDesc = "Upon eating an enemy piece, that piece becomes an ally Ghost for 2 turns. A ghost retains the piece's movement ability, but it does not retain its augmentation. It shares a space with its "killer" unless you chose to move it in your next turn.";
+    //     triggerVal = 3;
+    // }
+    public override void UseAugment()
+    {
+        
+    }
+}
