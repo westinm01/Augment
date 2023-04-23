4ea23601c25efeafcde8cf105121ce18f00aec34
myong003 <matthew.yong0801@gmail.com>
2023-04-22 18:09:26 -0700

    fixed pawn moving past pieces at start

--- Augment/Assets/Scripts/Chess Pieces/PawnPiece.cs
+++ Augment/Assets/Scripts/Chess Pieces/PawnPiece.cs
@@ -22,14 +22,13 @@ public class PawnPiece : ChessPiece
         if (GameManager.Instance.board.isValidMoveSpace(coord.x, nextY) && ValidMoveInCheck(nextMove))
         {
             possibleSpaces.Add(new Vector2Int(coord.x, nextY));
-        }
-
-        // If pawn is in starting position, also add another space
-        nextMove = new Vector2Int(coord.x, nextY - direction);
-        if ((coord.y == 1 && direction < 0) || (coord.y == GameManager.Instance.board.getHeight() - 2 && direction > 0))
-        {
-            if (GameManager.Instance.board.isValidMoveSpace(coord.x, nextY - direction) && ValidMoveInCheck(nextMove)){
-                possibleSpaces.Add(new Vector2Int(coord.x, nextY - direction));
+            // If pawn is in starting position, also add another space
+            nextMove = new Vector2Int(coord.x, nextY - direction);
+            if ((coord.y == 1 && direction < 0) || (coord.y == GameManager.Instance.board.getHeight() - 2 && direction > 0))
+            {
+                if (GameManager.Instance.board.isValidMoveSpace(coord.x, nextY - direction) && ValidMoveInCheck(nextMove)){
+                    possibleSpaces.Add(new Vector2Int(coord.x, nextY - direction));
+                }
             }
         }
         
